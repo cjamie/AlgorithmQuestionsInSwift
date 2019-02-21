7e5d2e804fcaec9206215e76026f72c70e41bcf8
Jamie Chu <jamiechu@Jamies-MacBook-Pro.local>
2019-02-21 01:14:25 -0800

    added leetcode49 - group algorithms

---
+++ Leetcode/49.swift
@@ -0,0 +1,28 @@
+import Foundation
+/*
+ 
+ algorithm:
+ 
+ 1: go through every string in the strs collection
+    2. sort each individual element to a string, and use it as a key for a hashmap
+        if it already exists, then append to existing array. otherwise, create a new array with that element as its only value
+ 
+ */
+
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        
+        var dict: [String: [String]] = [:]
+        
+        strs.forEach {
+            let key = String($0.sorted())
+            if var existing = dict[key] {
+                existing.append($0)
+                dict[key] = existing
+            } else {
+                dict[key] = [$0]
+            }
+        }
+        return dict.map{$0.value}
+    }
+}
